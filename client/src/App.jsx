5ea0206cadab178c5b2695fdbfb7622408d19c33
Shubham Pandey <shubhamkp54@gmail.com>
2023-10-26 15:38:53 +0530

    url Changed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
 
     const fetchBotResponse = async () => {
         const { data } = await axios.post(
-            "http://localhost:4000",
+            "https://chatgpt-app-r4rj.onrender.com",
             // "http://localhost:5173",
             { input },
             {
